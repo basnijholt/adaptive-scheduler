19dd2eb8f42c10bc93b6f95b128ca5103593f2cd
Bas Nijholt <basnijholt@gmail.com>
2019-05-13 17:17:30 -0700

    add _scheduler import

--- adaptive_scheduler/__init__.py
+++ adaptive_scheduler/__init__.py
@@ -1,4 +1,4 @@
-from adaptive_scheduler import client_support, pbs, server_support, slurm
+from adaptive_scheduler import client_support, pbs, server_support, slurm, _scheduler
 from adaptive_scheduler._version import __version__  # noqa: F401
 
-__all__ = ["client_support", "pbs", "server_support", "slurm"]
+__all__ = ["client_support", "pbs", "server_support", "slurm", "_scheduler"]
