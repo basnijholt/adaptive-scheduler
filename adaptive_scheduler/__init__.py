bfc39212f5759d7d65cc3c3432f5a7dcb06c4385
Bas Nijholt <basnijholt@gmail.com>
2019-05-23 10:09:08 -0700

    import cancel as cancel_jobs

--- adaptive_scheduler/__init__.py
+++ adaptive_scheduler/__init__.py
@@ -1,11 +1,5 @@
-from adaptive_scheduler import (
-    client_support,
-    pbs,
-    server_support,
-    slurm,
-    utils,
-    _scheduler,
-)
+from adaptive_scheduler import client_support, pbs, server_support, slurm, utils
+from adaptive_scheduler._scheduler import cancel as cancel_jobs
 from adaptive_scheduler._version import __version__  # noqa: F401
 
-__all__ = ["client_support", "pbs", "server_support", "slurm", "utils", "_scheduler"]
+__all__ = ["client_support", "pbs", "server_support", "slurm", "utils", "cancel_jobs"]
