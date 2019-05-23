14f00692f235c88788f34fd55b57761ac39546e5
Bas Nijholt <basnijholt@gmail.com>
2019-05-23 09:49:26 -0700

    simplify imports

--- adaptive_scheduler/_scheduler.py
+++ adaptive_scheduler/_scheduler.py
@@ -17,27 +17,17 @@ import os
 DEFAULT = "SLURM"
 
 scheduler_system = os.environ.get("SCHEDULER_SYSTEM", DEFAULT)
-if scheduler_system == "SLURM":
-    from adaptive_scheduler.slurm import (
-        get_job_id,
-        make_job_script,
-        queue,
-        ext,
-        submit_cmd,
-        cancel,
-    )
-elif scheduler_system == "PBS":
-    from adaptive_scheduler.pbs import (
-        get_job_id,
-        make_job_script,
-        queue,
-        ext,
-        submit_cmd,
-        cancel,
-    )
-else:
+if scheduler_system not in ("PBS", "SLURM"):
     raise NotImplementedError(
         f"SCHEDULER_SYSTEM={scheduler_system} is not implemented. Use SLURM or PBS."
     )
 
-__all__ = ["ext", "get_job_id", "make_job_script", "queue", "submit_cmd", "cancel"]
+names = ["ext", "get_job_id", "make_job_script", "queue", "submit_cmd", "cancel"]
+
+for name in names:
+    module = __import__(
+        f"adaptive_scheduler.{scheduler_system.lower()}", fromlist=[name]
+    )
+    globals()[name] = getattr(module, name)
+
+__all__ = names
