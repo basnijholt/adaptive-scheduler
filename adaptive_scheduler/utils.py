22efe7856a7471c899875a682530752f05d6f59f
Bas Nijholt <basnijholt@gmail.com>
2019-05-29 21:50:36 +0000

    add a progress bar for canceling jobs

--- adaptive_scheduler/utils.py
+++ adaptive_scheduler/utils.py
@@ -6,6 +6,8 @@ import warnings
 
 import adaptive
 import toolz
+from adaptive.notebook_integration import in_ipynb
+from tqdm import tqdm, tqdm_notebook
 
 
 def shuffle_list(*lists, seed=0):
@@ -49,16 +51,34 @@ def split_in_balancing_learners(learners, fnames, n_parts, strategy="npoints"):
     return new_learners, new_fnames
 
 
+def _progress(seq, with_progress_bar, desc=""):
+    if not with_progress_bar:
+        return seq
+    else:
+        if in_ipynb():
+            return tqdm_notebook(list(seq), desc=desc)
+        else:
+            return tqdm(list(seq), desc=desc)
+
+
 def _cancel_function(cancel_cmd, queue_function):
-    def cancel(job_names):
-        """Cancel all jobs in `job_names`."""
+    def cancel(job_names, with_progress_bar=True):
+        """Cancel all jobs in `job_names`.
+
+        Parameters
+        ----------
+        job_names : str
+            List of job names.
+        with_progress_bar : bool, default False
+            Display a progress bar using `tqdm`.
+        """
         job_names = set(job_names)
         to_cancel = [
             job_id
             for job_id, info in queue_function().items()
             if info["name"] in job_names
         ]
-        for job_id in to_cancel:
+        for job_id in _progress(to_cancel, with_progress_bar, "Canceling jobs"):
             cmd = f"{cancel_cmd} {job_id}"
             returncode = subprocess.run(cmd.split(), stderr=subprocess.PIPE).returncode
             if returncode != 0:
