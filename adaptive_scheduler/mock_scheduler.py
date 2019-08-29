5f20645f5ba8ff398a1c89cefddecc6c995a1151
Bas Nijholt <basnijholt@gmail.com>
2019-08-29 11:50:47 +0200

    fix small bugs in MockScheduler

--- adaptive_scheduler/mock_scheduler.py
+++ adaptive_scheduler/mock_scheduler.py
@@ -1,18 +1,19 @@
 import asyncio
+import datetime
 import logging
+import os
 import subprocess
-from typing import Any, Coroutine, Tuple
+from typing import Coroutine, Tuple
 
 import structlog
 import zmq
-
-from adaptive_scheduler.server_support import get_allowed_url
+import zmq.asyncio
 
 logger = logging.getLogger("adaptive_scheduler.mock_scheduler")
 logger.setLevel(logging.INFO)
 log = structlog.wrap_logger(logger)
 
-ctx = zmq.Context()
+DEFAULT_URL = "tcp://127.0.0.1:60547"
 
 
 class MockScheduler:
@@ -22,8 +23,9 @@ class MockScheduler:
         max_running_jobs=4,
         refresh_interval=0.1,
         python_executable="python",
+        url=None,
     ):
-        self.current_queue = {}
+        self._current_queue = {}
         self._job_id = 0
         self.max_running_jobs = max_running_jobs
         self.startup_delay = startup_delay
@@ -31,42 +33,57 @@ class MockScheduler:
         self.python_executable = python_executable
         self.ioloop = asyncio.get_event_loop()
         self.refresh_task = self.ioloop.create_task(self._refresh_coro())
-        self.url = get_allowed_url()
+        self.url = url or DEFAULT_URL
         self.command_listener_task = self.ioloop.create_task(self._command_listener())
 
-    def queue(self, only_me=True):
-        return self.current_queue
+    def queue(self, only_me: bool = True):
+        # only_me doesn't do anything, but the argument is there
+        # because it is in the other schedulers.
+        return self._current_queue
 
     def _queue_is_full(self):
-        n_running = sum(info["status"] == "R" for info in self.current_queue.values())
+        n_running = sum(info["status"] == "R" for info in self._current_queue.values())
         return n_running >= self.max_running_jobs
 
     def _get_new_job_id(self):
         job_id = self._job_id
         self._job_id += 1
-        return job_id
+        return str(job_id)
 
-    async def _submit_coro(self, job_id, fname):
+    async def _submit_coro(self, job_id: str, fname: str):
         await asyncio.sleep(self.startup_delay)
         while self._queue_is_full():
             await asyncio.sleep(self.refresh_interval)
         self._submit(job_id, fname)
 
-    def _submit(self, job_id, fname):
-        proc = subprocess.Popen([self.python_executable, fname], stdout=subprocess.PIPE)
-        info = self.current_queue[job_id]
-        info["proc"] = proc
-        info["status"] = "R"
+    def _submit(self, job_id: str, fname: str):
+        if job_id in self._current_queue:
+            # job_id could be cancelled before it started
+            proc = subprocess.Popen(
+                [self.python_executable, fname],
+                stdout=subprocess.PIPE,
+                env=dict(os.environ, JOB_ID=job_id),
+            )
+            info = self._current_queue[job_id]
+            info["proc"] = proc
+            info["status"] = "R"
 
-    def submit(self, job_name, fname):
+    def submit(self, job_name: str, fname: str):
         job_id = self._get_new_job_id()
-        self.current_queue[job_id] = {"job_name": job_name, "proc": None, "status": "P"}
+        self._current_queue[job_id] = {
+            "job_name": job_name,
+            "proc": None,
+            "status": "P",
+            "timestamp": str(datetime.datetime.now()),
+        }
         self.ioloop.create_task(self._submit_coro(job_id, fname))
         return job_id
 
-    def cancel(self, job_id):
-        info = self.current_queue.pop("job_id")
-        info["proc"].kill()
+    def cancel(self, job_id: str):
+        job_id = str(job_id)
+        info = self._current_queue.pop(job_id)
+        if info["proc"] is not None:
+            info["proc"].kill()
 
     async def _refresh_coro(self):
         while True:
@@ -77,12 +94,13 @@ class MockScheduler:
                 print(e)
 
     def refresh(self):
-        for job_id, info in self.current_queue.items():
+        for job_id, info in self._current_queue.items():
             if info["status"] == "R" and info["proc"].poll() is not None:
                 info["status"] = "F"
 
     async def _command_listener(self) -> Coroutine:
         log.debug("started _command_listener")
+        ctx = zmq.asyncio.Context()
         socket = ctx.socket(zmq.REP)
         socket.bind(self.url)
         try:
@@ -93,28 +111,31 @@ class MockScheduler:
         finally:
             socket.close()
 
-    def _dispatch(self, request: Tuple[str, Any]):
+    def _dispatch(self, request: Tuple[str, ...]):
         log.debug("got a request", request=request)
         request_type, *request_arg = request
         try:
             if request_type == "submit":
                 job_name, fname = request_arg
                 log.debug("submitting a task", fname=fname, job_name=job_name)
-                job_id = self.submit(fname)
+                job_id = self.submit(job_name, fname)
                 return job_id
             elif request_type == "cancel":
                 job_id = request_arg[0]
                 log.debug("got a cancel request", job_id=job_id)
                 self.cancel(job_id)
+                return None
             elif request_type == "queue":
                 log.debug("got a queue request")
                 return self.queue()
+            else:
+                log.debug("got unknown request")
         except Exception as e:
             return e
 
 
 def submit(job_name: str, fname: str, url: str) -> None:
-    with ctx.socket(zmq.REQ) as socket:
+    with zmq.Context().socket(zmq.REQ) as socket:
         socket.connect(url)
         socket.send_pyobj(("submit", job_name, fname))
         job_id = socket.recv_pyobj()
@@ -122,7 +143,7 @@ def submit(job_name: str, fname: str, url: str) -> None:
 
 
 def cancel(job_id: str, url: str) -> None:
-    with ctx.socket(zmq.REQ) as socket:
+    with zmq.Context().socket(zmq.REQ) as socket:
         socket.connect(url)
         socket.send_pyobj(("cancel", job_id))
         socket.recv_pyobj()
@@ -130,8 +151,28 @@ def cancel(job_id: str, url: str) -> None:
 
 
 def queue(url: str) -> None:
-    with ctx.socket(zmq.REQ) as socket:
+    with zmq.Context().socket(zmq.REQ) as socket:
         socket.connect(url)
         socket.send_pyobj(("queue",))
         queue = socket.recv_pyobj()
         print(queue)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--queue", action="store_true")
+    parser.add_argument("--submit", action="store", nargs=2, type=str)
+    parser.add_argument("--cancel", action="store", type=str)
+    parser.add_argument("--url", action="store", type=str, default=DEFAULT_URL)
+    args = parser.parse_args()
+
+    if args.queue:
+        queue(args.url)
+    elif args.submit:
+        job_name, fname = args.submit
+        submit(job_name, fname, args.url)
+    elif args.cancel:
+        job_id = args.cancel
+        cancel(job_id, args.url)
