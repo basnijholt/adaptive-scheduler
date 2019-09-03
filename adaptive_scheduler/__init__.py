38f6338dcc67adf53a3f2cb856f07a135a695ea3
Bas Nijholt <basnijholt@gmail.com>
2019-09-03 12:05:38 +0200

    put PBS, SLURM, and RunManager under main namespace

--- adaptive_scheduler/__init__.py
+++ adaptive_scheduler/__init__.py
@@ -1,4 +1,14 @@
 from adaptive_scheduler import client_support, scheduler, server_support, utils
 from adaptive_scheduler._version import __version__  # noqa: F401
+from adaptive_scheduler.scheduler import PBS, SLURM
+from adaptive_scheduler.server_support import RunManager
 
-__all__ = ["client_support", "scheduler", "server_support", "utils"]
+__all__ = [
+    "client_support",
+    "PBS",
+    "RunManager",
+    "scheduler",
+    "server_support",
+    "SLURM",
+    "utils",
+]
