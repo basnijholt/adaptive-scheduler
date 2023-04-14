45d518cc84ffbb559a2e4b9e27eb56c687433adf
Bas Nijholt <bas@nijho.lt>
2023-04-13 18:01:51 -0700

    fix tests

--- tests/test_server_support.py
+++ tests/test_server_support.py
@@ -7,18 +7,18 @@ from adaptive import Learner1D
 from tinydb import Query, TinyDB
 
 from adaptive_scheduler._mock_scheduler import MockScheduler
-from adaptive_scheduler.server_support import DatabaseManager
+from adaptive_scheduler.server_support import DatabaseManager, get_allowed_url
 from adaptive_scheduler.utils import _deserialize, _serialize, smart_goal
 
 
 @pytest.fixture
 def mock_scheduler():
-    return MockScheduler()
+    return MockScheduler(url=get_allowed_url())
 
 
 @pytest.fixture
 def db_manager(mock_scheduler, learners_and_fnames):
-    url = "tcp://127.0.0.1:5555"
+    url = get_allowed_url()
     db_fname = "test_db.json"
     learners, fnames = learners_and_fnames
     return DatabaseManager(url, mock_scheduler, db_fname, learners, fnames)
@@ -110,46 +110,103 @@ async def test_database_manager_dispatch_start_stop(db_manager, learners_and_fna
         assert entry["is_done"] is True
 
 
-async def send_message(db_manager, message):
-    ctx = zmq.asyncio.Context.instance()
-    socket = ctx.socket(zmq.REQ)
-    socket.connect(db_manager.url)
+async def send_message(socket, message):
     await socket.send_serialized(message, _serialize)
     response = await socket.recv_serialized(_deserialize)
-    socket.close()
     return response
 
 
+@pytest.fixture(scope="function")
+def socket(db_manager):
+    ctx = zmq.asyncio.Context.instance()
+    socket = ctx.socket(zmq.REQ)
+    socket.connect(db_manager.url)
+    yield socket
+    socket.close()
+
+
 @pytest.mark.asyncio
-async def test_database_manager_update(
-    db_manager: DatabaseManager, mock_scheduler, learners_and_fnames
-):
+async def test_database_manager_start_and_update(socket, db_manager: DatabaseManager):
     db_manager.create_empty_db()
-
-    queue = {"1000": {"job_id": "1000"}}
-    db_manager.update(queue)
-
-    # with TinyDB(db_manager.db_fname) as db:
-    #     Entry = Query()
-    #     entry = db.get(Entry.fname == "learner1.pkl")
-    #     assert entry["job_id"] == "1000", entry
-
-    # Start the DatabaseManager
-    asyncio.create_task(db_manager._manage())
+    db_manager.start()
+    await asyncio.sleep(0.1)  # Give it some time to start
 
     # Send a start message to the DatabaseManager
     job_id, log_fname, job_name = "1000", "log.log", "job_name"
     start_message = ("start", job_id, log_fname, job_name)
-    fname = await send_message(db_manager, start_message)
+    fname = await send_message(socket, start_message)
 
     # Check if the correct fname is returned
     assert fname == "learner1.pkl", fname
 
+    # Check that the database is updated correctly
+    with TinyDB(db_manager.db_fname) as db:
+        Entry = Query()
+        entry = db.get(Entry.fname == fname)
+        assert entry["job_id"] == job_id
+        assert entry["log_fname"] == log_fname
+        assert entry["job_name"] == job_name
+
+    # Say that the job is still running
+    queue = {"1000": {"job_id": "1000"}}
+    db_manager.update(queue)
+
+    # Check that the database is the same
+    with TinyDB(db_manager.db_fname) as db:
+        Entry = Query()
+        entry = db.get(Entry.fname == fname)
+        assert entry["job_id"] == job_id
+        assert entry["log_fname"] == log_fname
+        assert entry["job_name"] == job_name
+
+    # Say that the job is died
+    queue = {}
+    db_manager.update(queue)
+
+    # Check that the database is updated correctly
+    with TinyDB(db_manager.db_fname) as db:
+        Entry = Query()
+        entry = db.get(Entry.fname == fname)
+        assert entry["job_id"] is None
+
+
+@pytest.mark.asyncio
+async def test_database_manager_start_stop(socket, db_manager: DatabaseManager):
+    db_manager.create_empty_db()
+    # Start the DatabaseManager
+    db_manager.start()
+    await asyncio.sleep(0.1)  # Give it some time to start
+    assert db_manager.task is not None
+
+    # Send a start message to the DatabaseManager
+    job_id, log_fname, job_name = "1000", "log.log", "job_name"
+    start_message = ("start", job_id, log_fname, job_name)
+    fname = await send_message(socket, start_message)
+
+    # Check if the correct fname is returned
+    assert fname == "learner1.pkl", fname
+
+    # Check that the database is updated correctly
+    with TinyDB(db_manager.db_fname) as db:
+        Entry = Query()
+        entry = db.get(Entry.fname == fname)
+        assert entry["job_id"] == job_id
+        assert entry["log_fname"] == log_fname
+        assert entry["job_name"] == job_name
+
+    # Check that task is still running
+    assert db_manager.task is not None
+    assert not db_manager.task.done()
+
     # Send a stop message to the DatabaseManager
     stop_message = ("stop", fname)
-    await send_message(db_manager, stop_message)
+    reply = await send_message(socket, stop_message)
+    assert reply is None
 
     with TinyDB(db_manager.db_fname) as db:
         Entry = Query()
         entry = db.get(Entry.fname == "learner1.pkl")
         assert entry["job_id"] is None
+
+    msg = await send_message(socket, start_message)
+    assert msg == "learner2.pkl"
