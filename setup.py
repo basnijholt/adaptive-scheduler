1d74fa01629c32616f9756c5a1beefc7e986ab75
Bas Nijholt <bas@nijho.lt>
2023-04-14 00:57:41 -0700

    Require Python 3.9

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     name="adaptive_scheduler",
     version=version,
     cmdclass=cmdclass,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     packages=find_packages(),
     include_package_data=True,
     maintainer="Bas Nijholt",
