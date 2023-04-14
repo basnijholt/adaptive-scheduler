550304b43cb614e55ca9a00e1cba1f0968f6ec1f
Bas Nijholt <bas@nijho.lt>
2023-04-14 00:54:16 -0700

    right logs only

--- tests/test_log_generation.py
+++ tests/test_log_generation.py
@@ -8,12 +8,14 @@ import pytest
 from adaptive_scheduler import client_support
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_get_log_entry(learners):
     # Prepare the runner and the learner
     learner = learners[0]
     runner = adaptive.Runner(
-        learner, npoints_goal=1000, executor=adaptive.runner.SequentialExecutor()
+        learner,
+        npoints_goal=1000,
+        executor=adaptive.runner.SequentialExecutor(),
     )
     while learner.npoints < 100:
         learner.loss()  # populate cache
@@ -29,10 +31,10 @@ async def test_get_log_entry(learners):
         "cpu_usage",
         "mem_usage",
     ]
-    assert all([key in result for key in expected_keys])
+    assert all(key in result for key in expected_keys)
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_log_info(learners, caplog):
     # Prepare the runner and the learner
     learner = learners[0]
@@ -46,10 +48,17 @@ async def test_log_info(learners, caplog):
     # Wait for some time to let the logging happen
     await asyncio.sleep(2)
 
+    # Filter the captured log records based on level and logger name
+    filtered_records = [
+        r
+        for r in caplog.records
+        if r.levelno == logging.INFO and r.name == "adaptive_scheduler.client"
+    ]
+
     # Check if the captured log records contain expected log entries
-    assert len(caplog.records) > 0
+    assert len(filtered_records) > 0
     current_status_entries = 0
-    for record in caplog.records:
+    for record in filtered_records:
         msg = record.getMessage()
         log_entry = json.loads(msg)
         if "current status" in log_entry["event"]:
@@ -62,7 +71,7 @@ async def test_log_info(learners, caplog):
                 "cpu_usage",
                 "mem_usage",
             ]
-            assert all([key in log_entry for key in expected_keys])
+            assert all(key in log_entry for key in expected_keys)
 
     # Check if there were any "current status" log entries
     assert current_status_entries > 0
