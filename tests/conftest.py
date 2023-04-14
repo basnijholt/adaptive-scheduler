bc50d0b8076b64eb5ec1b0b7720d578598567ee6
Bas Nijholt <bas@nijho.lt>
2023-04-14 12:14:00 -0700

    start conftext fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+"""Tests for conftest module."""
+
 import textwrap
+from pathlib import Path
 
 import pytest
 import zmq.asyncio
@@ -13,6 +16,8 @@ from adaptive_scheduler.server_support import (
 
 
 class MockScheduler(BaseScheduler):
+    """A mock scheduler for testing purposes."""
+
     _ext = ".mock"
     _submit_cmd = "echo"
     _options_flag = "#MOCK"
@@ -25,11 +30,12 @@ class MockScheduler(BaseScheduler):
         self._job_id = 0
 
     def queue(self, me_only: bool = True) -> dict[str, dict]:
-        # Return a fake queue for demonstration purposes
+        """Return a fake queue for demonstration purposes."""
         print("Mock queue:", self._queue_info)
         return self._queue_info
 
     def job_script(self) -> str:
+        """Return a job script for the mock scheduler."""
         return textwrap.dedent(
             f"""\
             {self.extra_scheduler}
@@ -42,6 +48,7 @@ class MockScheduler(BaseScheduler):
         )
 
     def start_job(self, name: str) -> None:
+        """Start a mock job."""
         print("Starting a mock job:", name)
         self._started_jobs.append(name)
         self._queue_info[str(self._job_id)] = {
@@ -57,18 +64,21 @@ class MockScheduler(BaseScheduler):
         with_progress_bar: bool = True,  # noqa: FBT001
         max_tries: int = 5,
     ) -> None:
+        """Cancel mock jobs."""
         print("Canceling mock jobs:", job_names)
         for job_name in job_names:
             self._queue_info.pop(job_name, None)
             self._started_jobs.remove(job_name)
 
     def update_queue(self, job_name: str, status: str) -> None:
+        """Update the queue with the given job_name and status."""
         self._queue_info[job_name] = {"job_name": job_name, "status": status}
 
 
 @pytest.fixture()
-def mock_scheduler(tmpdir) -> MockScheduler:
-    return MockScheduler(log_folder=str(tmpdir), cores=8)
+def mock_scheduler(tmp_path: Path) -> MockScheduler:
+    """Fixture for creating a MockScheduler instance."""
+    return MockScheduler(log_folder=str(tmp_path), cores=8)
 
 
 @pytest.fixture()
@@ -77,18 +87,21 @@ def db_manager(
     learners: list[Learner1D],
     fnames: list[str],
     tmp_path,
-):
+) -> DatabaseManager:
+    """Fixture for creating a DatabaseManager instance."""
     url = get_allowed_url()
     db_fname = str(tmp_path / "test_db.json")
     return DatabaseManager(url, mock_scheduler, db_fname, learners, fnames)
 
 
-def func(x):
+def func(x: float) -> float:
+    """A simple quadratic function."""
     return x**2
 
 
 @pytest.fixture()
 def learners() -> list[Learner1D]:
+    """Fixture for creating a list of Learner1D instances."""
     learner1 = Learner1D(func, bounds=(-1, 1))
     learner2 = Learner1D(func, bounds=(-1, 1))
     learners = [learner1, learner2]
@@ -96,13 +109,15 @@ def learners() -> list[Learner1D]:
 
 
 @pytest.fixture()
-def fnames(learners, tmpdir) -> list[str]:
-    fnames = [str(tmpdir / f"learner{i}.pkl") for i, _ in enumerate(learners)]
+def fnames(learners: list[Learner1D], tmp_path: Path) -> list[str]:
+    """Fixture for creating a list of filenames for learners."""
+    fnames = [str(tmp_path / f"learner{i}.pkl") for i, _ in enumerate(learners)]
     return fnames
 
 
 @pytest.fixture()
 def socket(db_manager: DatabaseManager) -> zmq.asyncio.Socket:
+    """Fixture for creating a ZMQ socket."""
     ctx = zmq.asyncio.Context.instance()
     socket = ctx.socket(zmq.REQ)
     socket.connect(db_manager.url)
@@ -115,5 +130,6 @@ def job_manager(
     db_manager: DatabaseManager,
     mock_scheduler: MockScheduler,
 ) -> JobManager:
+    """Fixture for creating a JobManager instance."""
     job_names = ["job1", "job2"]
     return JobManager(job_names, db_manager, mock_scheduler, interval=0.05)
