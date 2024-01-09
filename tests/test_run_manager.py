8e01b32182310b09f9146f0b091016127af10a3f
Bas Nijholt <bas@nijho.lt>
2024-01-09 16:05:44 -0500

    fix test

--- tests/test_run_manager.py
+++ tests/test_run_manager.py
@@ -223,7 +223,7 @@ async def test_start_one_by_one(
 
 
 @pytest.mark.asyncio()
-async def test_run_manager_auto_restart(
+async def test_run_manager_auto_restart(  # noqa: PLR0915
     mock_scheduler: MockScheduler,
     learners: list[adaptive.Learner1D]
     | list[adaptive.BalancingLearner]
@@ -241,30 +241,28 @@ async def test_run_manager_auto_restart(
     # Note: we don't know whether it is:
     # This: "0": {"job_name": "adaptive-scheduler-0"}, "1": {"job_name": "adaptive-scheduler-1"}
     # or:   "1": {"job_name": "adaptive-scheduler-0"}, "0": {"job_name": "adaptive-scheduler-1"}
-    job_name0, job_name1 = "adaptive-scheduler-0", "adaptive-scheduler-1"
+    job_names = job_name0, job_name1 = "adaptive-scheduler-0", "adaptive-scheduler-1"
     job_id0, job_id1 = rm.scheduler.job_names_to_job_ids(job_name0, job_name1)
+    log_fnames = ("log0.log", "log1.log")
 
     db = rm.database_manager.as_dicts()
     assert db[0]["job_id"] is None  # no jobs are started yet
 
     # Send a start message to the DatabaseManager
     # This is coming from the client
-    jobs = [
-        (job_id0, "log0.log", job_name0),
-        (job_id1, "log1.log", job_name1),
-    ]
-
     with get_socket(rm.database_manager) as socket:
-        for job_id, log_fname, job_name in jobs:
-            rm.database_manager._choose_fname(job_name)
+        for i, (entry, log_fname) in enumerate(zip(db, log_fnames)):
+            job_id = str(i)
+            job_name = entry["job_name"]
             start_message = ("start", job_id, log_fname, job_name)
             await send_message(socket, start_message)
 
     db = rm.database_manager.as_dicts()
-    for i, (job_id, _, job_name) in enumerate(jobs):
-        assert db[i]["job_id"] == job_id
-        assert db[i]["job_name"] == job_name
-        assert not db[i]["is_done"]
+    assert len(db) == 2
+    for entry in db:
+        assert entry["job_id"] in ("0", "1")
+        assert entry["job_name"] in job_names
+        assert not entry["is_done"]
 
     # Mark the first job as cancelled
     rm.scheduler.update_queue(job_name0, "C")  # type: ignore[attr-defined]
@@ -278,7 +276,7 @@ async def test_run_manager_auto_restart(
     }
     rm.scheduler._queue_info.pop(job_id0)
     await asyncio.sleep(0.15)
-    # Check that the job is restarted automatically with a new job_id:
+    # Check that the job is restarted automatically with a new job_id:_
     q = rm.scheduler.queue()
     assert q["2"] == {"job_name": job_name0, "state": "RUNNING", "status": "R"}
 
