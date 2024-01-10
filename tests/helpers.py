77166b0a14e93c81ae7f8375dd1b3c27fd4f625e
Bas Nijholt <bas@nijho.lt>
2024-01-10 15:42:10 -0500

    test

--- tests/helpers.py
+++ tests/helpers.py
@@ -70,7 +70,7 @@ class MockScheduler(BaseScheduler):
             """,
         )
         return job_script.format(
-            extra_scheduler=self.extra_scheduler,
+            extra_scheduler=self.extra_scheduler(index=index),
             extra_env_vars=self.extra_env_vars,
             extra_script=self.extra_script,
             executor_specific=self._executor_specific("${NAME}", options, index=index),
